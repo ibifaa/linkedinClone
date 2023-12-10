890201ee7c30d1bf481df5e1f5a011c0434aa6fe
devibifaa <ib2link@yahoo.com>
2023-12-10 04:54:19 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>LinkedIn clone</h1>
+
+     {/* Header */}
+     <Header />
+
+     {/* Body */}
+     {/* Sidebar */}
+     {/* Feeds */}
+     {/* Widget */}
+    </div>
+  );
+}
+
+export default App;
